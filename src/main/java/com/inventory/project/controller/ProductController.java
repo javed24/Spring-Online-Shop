45db5d30cd1e27a480847881a2c2581cb96dac0e
javed24 <aquib_javed@live.com>
2018-04-13 15:25:24 -0400

    added mapping for POST request

--- src/main/java/com/inventory/project/controller/ProductController.java
+++ src/main/java/com/inventory/project/controller/ProductController.java
@@ -1,33 +1,47 @@
 package com.inventory.project.controller;
 
-import com.inventory.project.model.Product;
-import com.inventory.project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import com.inventory.project.dao.ProductDao;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import com.inventory.project.model.Product;
-import com.inventory.project.service.CartService;
 import com.inventory.project.service.ProductService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ProductController {
     @Autowired
     private ProductService ps;
 
+
     @RequestMapping("/")
-    public String listProducts(ModelMap modelMap){
-        List<Product> products = new ArrayList<>();
-        products = ps.getAll();
-        modelMap.addAttribute("products", products);
-        System.out.print(">>>>>>>>>"+products.get(0).getProductname());
+    public String listProducts(ModelMap model){
+        List<Product> products = ps.getAll();
+        model.addAttribute("products", products);
+        model.addAttribute("productAtt", new Product());
+        System.out.print(">>>>>>>>"+products.get(0).getProductName());
         return "home";
+
     }
+
+    @RequestMapping(value="/", method = RequestMethod.POST)
+    public String addProduct(@ModelAttribute("productAtt") Product product,BindingResult result) {
+        if (result.hasErrors()){
+            return "home";
+        }
+        else {
+            System.out.println("name is:>>>>>>>> " + product.getProductName());
+            //ps.save(product);
+            return "redirect:";
+        }
+
+    }
+
 }
